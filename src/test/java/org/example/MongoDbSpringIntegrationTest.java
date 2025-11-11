f4418f12eabe9fff82cc0960ae909d38d9fd04b2
Fabian Kreis <f.kreis@enbw.com>
2025-11-11 09:51:08 +0100

    Tests cases with MongoCollection

--- src/test/java/org/example/MongoDbSpringIntegrationTest.java
+++ src/test/java/org/example/MongoDbSpringIntegrationTest.java
@@ -1,7 +1,6 @@
 package org.example;
 
-import com.mongodb.BasicDBObjectBuilder;
-import com.mongodb.DBObject;
+import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +17,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @DataMongoTest
 @ExtendWith(SpringExtension.class)
 @TestPropertySource(properties = {
@@ -30,16 +31,20 @@ public class MongoDbSpringIntegrationTest {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    private MongoCollection<Document> collection;
+
     @BeforeEach
     public void setUp() {
         // Clear any previous data
         mongoTemplate.dropCollection(COLLECTIONNAME);
+        collection = mongoTemplate.getDb().getCollection(COLLECTIONNAME);
+        assertNotNull(collection, "MongoCollection should not be null");
 
         List<Document> docs = new ArrayList<>();
 
         // ObjectId
-        docs.add(new Document("_id", new ObjectId())
-                .append("value", new ObjectId()));
+        docs.add(new Document("_id", new ObjectId("507f1f77bcf86cd799439011"))
+                .append("value", new ObjectId("64b8b3e9fa9c2b5d6f4e1234")));
 
         // String
         docs.add(new Document("_id", "string_id")
@@ -62,30 +67,144 @@ public class MongoDbSpringIntegrationTest {
                 .append("value", false));
 
         // Date
-        docs.add(new Document("_id", new Date())
-                .append("value", new Date(System.currentTimeMillis() + 60000)));
+        docs.add(new Document("_id", new Date(123456))
+                .append("value", new Date(654321)));
 
         // UUID
-        docs.add(new Document("_id", UUID.randomUUID())
-                .append("value", UUID.randomUUID()));
+        docs.add(new Document("_id", UUID.fromString("123e4567-e89b-12d3-a456-426614174000"))
+                .append("value", UUID.fromString("987e6543-e21b-12d3-a456-426614174999")));
 
         // Insert all
         mongoTemplate.insert(docs, COLLECTIONNAME);
+    }
 
-        System.out.println("✅ Inserted " + docs.size() + " documents into collection '" + COLLECTIONNAME + "'");
+    // #################################################################################################################
+    // Test cases using com.mongodb.client.MongoCollection
+    // #################################################################################################################
+
+    // -------------------------------
+    // ObjectId
+    // -------------------------------
+    @Test
+    void testFindByObjectId() {
+        Document doc = collection.find(new Document().append("_id", new ObjectId("507f1f77bcf86cd799439011"))).first();
+        assertNotNull(doc);
     }
 
     @Test
-    public void test() {
-        // given
-        DBObject objectToSave = BasicDBObjectBuilder.start()
-                .add("key", "value")
-                .get();
-
-        // when
-        DBObject collection = mongoTemplate.save(objectToSave, "collection");
-
-        // then
-        System.out.println(collection);
+    void testFindByObjectIdValue() {
+        Document doc = collection.find(new Document().append("value", new ObjectId("64b8b3e9fa9c2b5d6f4e1234"))).first();
+        assertNotNull(doc);
     }
+
+    // -------------------------------
+    // String
+    // -------------------------------
+    @Test
+    void testFindByStringId() {
+        Document doc = collection.find(new Document().append("_id", "string_id")).first();
+        assertNotNull(doc);
+    }
+
+    @Test
+    void testFindByStringValue() {
+        Document doc = collection.find(new Document().append("value", "string_value")).first();
+        assertNotNull(doc);
+    }
+
+    // -------------------------------
+    // Integer
+    // -------------------------------
+    @Test
+    void testFindByIntegerId() {
+        Document doc = collection.find(new Document().append("_id", 123)).first();
+        assertNotNull(doc);
+    }
+
+    @Test
+    void testFindByIntegerValue() {
+        Document doc = collection.find(new Document().append("value", 456)).first();
+        assertNotNull(doc);
+    }
+
+    // -------------------------------
+    // Long
+    // -------------------------------
+    @Test
+    void testFindByLongId() {
+        Document doc = collection.find(new Document().append("_id", 1234567890123L)).first();
+        assertNotNull(doc);
+    }
+
+    @Test
+    void testFindByLongValue() {
+        Document doc = collection.find(new Document().append("value", 9876543210987L)).first();
+        assertNotNull(doc);
+    }
+
+    // -------------------------------
+    // Double
+    // -------------------------------
+    @Test
+    void testFindByDoubleId() {
+        Document doc = collection.find(new Document().append("_id", 3.14159)).first();
+        assertNotNull(doc);
+    }
+
+    @Test
+    void testFindByDoubleValue() {
+        Document doc = collection.find(new Document().append("value", 2.71828)).first();
+        assertNotNull(doc);
+    }
+
+    // -------------------------------
+    // Boolean
+    // -------------------------------
+    @Test
+    void testFindByBooleanId() {
+        Document doc = collection.find(new Document().append("_id", true)).first();
+        assertNotNull(doc);
+    }
+
+    @Test
+    void testFindByBooleanValue() {
+        Document doc = collection.find(new Document().append("value", false)).first();
+        assertNotNull(doc);
+    }
+
+    // -------------------------------
+    // Date
+    // -------------------------------
+    @Test
+    void testFindByDateId() {
+        Document doc = collection.find(new Document().append("_id", new Date(123456))).first();
+        assertNotNull(doc);
+    }
+
+    @Test
+    void testFindByDateValue() {
+        Document doc = collection.find(new Document().append("value", new Date(654321))).first();
+        assertNotNull(doc);
+    }
+
+    // -------------------------------
+    // UUID
+    // -------------------------------
+    @Test
+    void testFindByUUIDId() {
+        Document doc = collection.find(new Document().append("_id", UUID.fromString("123e4567-e89b-12d3-a456-426614174000"))).first();
+        assertNotNull(doc);
+    }
+
+    @Test
+    void testFindByUUIDValue() {
+        Document doc = collection.find(new Document().append("value", UUID.fromString("987e6543-e21b-12d3-a456-426614174999"))).first();
+        assertNotNull(doc);
+    }
+
+    // #################################################################################################################
+    // Test cases using org.springframework.data.mongodb.core.MongoTemplate
+    // #################################################################################################################
+
+    // ... TODO
 }
